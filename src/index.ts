411087d9a3228191a2e1bb2042b794aa9c5ff5ea
Damian Tarnawsky <damian@ionic.io>
2023-01-21 09:01:48 -0800

    Rename

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import { registerPlugin } from '@capacitor/core';
 
 import type { MDMAppConfigPlugin } from './definitions';
 
-const MDMAppConfig = registerPlugin<MDMAppConfigPlugin>(
+const AppConfig = registerPlugin<MDMAppConfigPlugin>(
   'MDMAppConfig',
   {},
 );
 
 export * from './definitions';
-export { MDMAppConfig };
+export { AppConfig };
